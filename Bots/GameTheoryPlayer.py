e6ee1ac11047b7bc0bb9ca517a0e11b9d4720400
HerBaBenedek <herbabenedek@gmail.com>
2023-12-27 14:35:40 +0000

    working basic weights

---
+++ Bots/GameTheoryPlayer.py
@@ -0,0 +1,62 @@
+from catanatron import Player
+from catanatron_experimental.cli.cli_players import register_player
+from catanatron.models.enums import *
+
+@register_player("GT")
+class GameTheoryPlayer(Player):
+
+
+
+    def __init__(self, color, is_bot=True, action_weights={}):
+       
+        super().__init__(color, is_bot)
+        self.action_weights = {
+            ActionType.ROLL : 0,
+            ActionType.MOVE_ROBBER : 0,
+            ActionType.DISCARD : 0,
+            ActionType.BUILD_ROAD : 10,
+            ActionType.BUILD_SETTLEMENT : 20,
+            ActionType.BUILD_CITY : 30,
+            ActionType.BUY_DEVELOPMENT_CARD : 10,
+            ActionType.PLAY_KNIGHT_CARD : 10,
+            ActionType.PLAY_YEAR_OF_PLENTY : 10,
+            ActionType.PLAY_MONOPOLY : 10,
+            ActionType.PLAY_ROAD_BUILDING : 10,
+            ActionType.MARITIME_TRADE : 0,
+            ActionType.OFFER_TRADE : 0,
+            ActionType.ACCEPT_TRADE : 0,
+            ActionType.REJECT_TRADE : 0,
+            ActionType.CONFIRM_TRADE : 0,
+            ActionType.CANCEL_TRADE : 0,
+            ActionType.END_TURN : 5  }
+
+        
+
+    def decide(self, game, playable_actions):
+        """Should return one of the playable_actions.
+
+        Args:
+            game (Game): complete game state. read-only.
+            playable_actions (Iterable[Action]): options to choose from
+        Return:
+            action (Action): Chosen element of playable_actions
+        """
+        print("\n")
+        print(playable_actions)
+        
+        if len(playable_actions) == 1:
+            return playable_actions[0]
+
+        best_action_value = 0
+
+        best_action = playable_actions[0]
+
+        for action in playable_actions:
+            
+            current_action_value = self.action_weights[action.action_type]
+
+            if current_action_value > best_action_value:
+                best_action_value = current_action_value
+                best_action = action
+
+        return best_action
--- measurements/baseline_run.sh
+++ measurements/baseline_run.sh
@@ -1,3 +1,4 @@
 catanatron-play --players=R,R,R,W --num=100
 catanatron-play --code=Bots/AlphaBetaPlayer.py --players=R,R,R,AlphaBeta --num=10
 catanatron-play --code=Bots/MiniMaxPlayer.py --players=R,R,R,MiniMax --num=1
+catanatron-play --code=Bots/GameTheoryPlayer.py --players=R,R,R,GT --num=1
